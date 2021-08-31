0c6b1fbee583227f03dddb02e8d55be51fdbe797
Anders Bjerner <abjerner@skybrud.dk>
2021-08-31 21:47:19 +0200

    Build stuff

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.Social.Toggl /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
+dotnet build src/Skybrud.Social.Toggl --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
