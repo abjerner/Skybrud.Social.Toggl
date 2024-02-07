b0e75179fc6a7abfb85ce96d7eef3e689fa74d35
Anders Bjerner <abjerner@skybrud.dk>
2024-02-07 22:05:10 +0100

    Internal HTTP stuff

--- src/Skybrud.Social.Toggl/Http/TogglHttpClient.cs
+++ src/Skybrud.Social.Toggl/Http/TogglHttpClient.cs
@@ -1,7 +1,9 @@
 ﻿using Skybrud.Essentials.Http;
 using Skybrud.Essentials.Http.Client;
+using Skybrud.Essentials.Http.Options;
 using Skybrud.Essentials.Security;
 using Skybrud.Social.Toggl.Apis;
+using Skybrud.Social.Toggl.Contants;
 
 namespace Skybrud.Social.Toggl.Http;
 
@@ -49,6 +51,31 @@ public class TogglHttpClient : HttpClient {
     /// <inheritdoc />
     protected override void PrepareHttpRequest(IHttpRequest request) {
         if (string.IsNullOrWhiteSpace(ApiToken) == false) request.Authorization = $"Basic {SecurityUtils.Base64Encode($"{ApiToken}:api_token")}";
+    }
+
+    /// <summary>
+    /// Returns the response of the request identified by the specified <paramref name="options"/>.
+    /// </summary>
+    /// <param name="options">The options for the request to the API.</param>
+    /// <returns>An instance of <see cref="IHttpResponse"/> representing the raw response.</returns>
+    public override IHttpResponse GetResponse(IHttpRequestOptions options) {
+
+        // Get the request based on the options
+        IHttpRequest request = options.GetRequest();
+
+        // Append the scheme and host if not already present
+        switch (options) {
+            case TogglTrackHttpRequestOptions:
+                if (request.Url.StartsWith("/")) request.Url = $"https://{TogglConstants.Track.HostName}{request.Url}";
+                break;
+        }
+
+        // Prepare the request
+        PrepareHttpRequest(request);
+
+        // Send the request to the API and return the response
+        return request.GetResponse();
+
     }
 
     #endregion
---
+++ src/Skybrud.Social.Toggl/Http/TogglTrackHttpRequestOptions.cs
@@ -0,0 +1,17 @@
+﻿using Skybrud.Essentials.Http;
+using Skybrud.Essentials.Http.Options;
+
+namespace Skybrud.Social.Toggl.Http;
+
+/// <summary>
+/// Options describing a request for the Toggl Track API.
+/// </summary>
+public abstract class TogglTrackHttpRequestOptions : IHttpRequestOptions {
+
+    /// <summary>
+    /// Returns a new <see cref="IHttpRequest"/> instance for this options instance.
+    /// </summary>
+    /// <returns>An instance of <see cref="IHttpRequest"/>.</returns>
+    public abstract IHttpRequest GetRequest();
+
+}
