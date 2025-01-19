920b3102438bf7a9bc1c013b2607a7e0f2db6d60
Anders Bjerner <abjerner@skybrud.dk>
2025-01-19 14:54:59 +0100

    #H5IS

--- src/Skybrud.Social.Toggl/Exceptions/TogglJsonParseException.cs
+++ src/Skybrud.Social.Toggl/Exceptions/TogglJsonParseException.cs
@@ -6,14 +6,14 @@ namespace Skybrud.Social.Toggl.Exceptions;
 /// <summary>
 /// Exception class indicating that parsing a <see cref="JObject"/> into a model for the Toggl implementation failed.
 /// </summary>
-public class TogglObjectParseException : TogglParseException<JToken> {
+public class TogglJsonParseException : TogglParseException<JToken> {
 
     /// <summary>
     /// Initializes a new instance based on the specified JSON <paramref name="value"/> and <paramref name="message"/>.
     /// </summary>
     /// <param name="value">The JSON value that could not be parsed.</param>
     /// <param name="message">The exception message.</param>
-    public TogglObjectParseException(JToken value, string message) : base(value, message) { }
+    public TogglJsonParseException(JToken value, string message) : base(value, message) { }
 
     /// <summary>
     /// Initializes a new instance based on the specified JSON <paramref name="value"/> and <paramref name="message"/>.
@@ -21,6 +21,6 @@ public class TogglObjectParseException : TogglParseException<JToken> {
     /// <param name="value">The value that could not be parsed.</param>
     /// <param name="message">The message of the exception.</param>
     /// <param name="innerException">The inner exception, if any.</param>
-    public TogglObjectParseException(JToken value, string message, Exception? innerException) : base(value, message, innerException) { }
+    public TogglJsonParseException(JToken value, string message, Exception? innerException) : base(value, message, innerException) { }
 
 }
